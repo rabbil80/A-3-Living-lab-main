328c4fa5af7dce2010e8a1d0fd645ed526913606
rabbil80 <mdr191700@gmail.com>
2025-01-31 02:47:25 +0600

    completed my header

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'], // Specify the files to scan for class names
+    theme: {
+      extend: {}, 
+    },
+    plugins: [], 
+  };
+  
